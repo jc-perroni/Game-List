6016353d52afba16b17a493f4b74ac629c4601fa
Julio Cesar Perroni <julio_veia@hotmail.com>
2024-11-14 17:40:29 -0300

    Project created

---
+++ src/main/java/com/juliop/gamesproject/GamesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.juliop.gamesproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliop/gamesproject/GamesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliop.gamesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
