b334bdab45f88b739be391f850a1523afcbab46f
Julio Cesar Perroni <julio_veia@hotmail.com>
2024-11-15 12:35:40 -0300

    Add Domain Model

---
+++ src/main/java/com/juliop/gamesproject/entities/GameList.java
@@ -0,0 +1,41 @@
+package com.juliop.gamesproject.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String Name;
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
